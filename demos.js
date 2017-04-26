3fe4a7f04c882db0e4ae677cae9ce41a2c3a872b
Michael Boreham <bor3ham@gmail.com>
2017-04-26 17:18:13 +1000

    Update demos to js not jsx

---
+++ demos.js
@@ -0,0 +1,6 @@
+import { init } from 'abas-demo-site'
+
+import FlatSimpleDemo from './flat-simple.js'
+import NestedEditableDemo from './nested-editable.js'
+
+init();
--- demos.jsx
+++
@@ -1,6 +0,0 @@
-import { init } from 'abas-demo-site'
-
-import FlatSimpleDemo from './flat-simple.jsx'
-import NestedEditableDemo from './nested-editable.jsx'
-
-init();
