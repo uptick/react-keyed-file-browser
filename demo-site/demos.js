7f07ffdbb6f79bbabc1c22c53340006a5d9d0193
Jarek Glowacki <jarekwg@gmail.com>
2018-01-18 14:10:44 +1100

    Repoint to uptick-demo-site

--- demo-site/demos.js
+++ demo-site/demos.js
@@ -1,4 +1,4 @@
-import { init } from 'abas-demo-site'
+import { init } from 'uptick-demo-site'
 
 import FlatSimpleDemo from './flat-simple.js'
 import NestedEditableDemo from './nested-editable.js'
