159b50072e5a0000eb4432a26d272848cf8b3918
Jarek Glowacki <jarekwg@gmail.com>
2018-02-08 15:44:12 +1100

    A start..

--- demo-site/demos.js
+++ demo-site/demos.js
@@ -2,5 +2,6 @@ import { init } from 'uptick-demo-site'
 
 import FlatSimpleDemo from './flat-simple.js'
 import NestedEditableDemo from './nested-editable.js'
+import GroupedThumbnailsDemo from './grouped-thumbnails.js'
 
 init();
