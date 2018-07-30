ae7eb10f8c2da438985bfc582ecbda212a6f7be6
Fiona Lawrence <fiona.lawrence@aba-systems.com.au>
2018-07-30 13:58:00 +1000

    Update demo

--- grouped-thumbnails.js
+++ grouped-thumbnails.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Moment from 'moment'
 
-import FileBrowser, { FileRenderers, FolderRenderers } from 'react-keyed-file-browser'
+import FileBrowser, { FileRenderers, FolderRenderers, Groupers } from 'react-keyed-file-browser'
 
 const mount = document.querySelectorAll('div.demo-mount-grouped-thumbnails')
 ReactDOM.render(
@@ -23,10 +23,36 @@ ReactDOM.render(
         modified: +Moment().subtract(3, 'days'),
         size: 52 * 1024,
       },
+      {
+        key: 'dog.png',
+        modified: +Moment().subtract(1, 'hours'),
+        size: 1.5 * 1024 * 1024,
+      },
+      {
+        key: 'turtle.png',
+        modified: +Moment().subtract(3, 'months'),
+        size: 545 * 1024,
+      },
+      {
+        key: 'gecko.png',
+        modified: +Moment().subtract(2, 'days'),
+        size: 52 * 1024,
+      },
+      {
+        key: 'centipede.png',
+        modified: +Moment().subtract(0.5, 'hours'),
+        size: 1.5 * 1024 * 1024,
+      },
+      {
+        key: 'possum.png',
+        modified: +Moment().subtract(32, 'days'),
+        size: 545 * 1024,
+      },
     ]}
     renderStyle="list"
     nestChildren
     headerRenderer={null}
+    group={Groupers.GroupByModifiedRelative}
     fileRenderer={FileRenderers.ListThumbnailFile}
     folderRenderer={FolderRenderers.ListThumbnailFolder}
   />,
