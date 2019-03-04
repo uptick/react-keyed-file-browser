a2eac16be542240a2c2cc26885c9421fe5f08143
jlo-1 <jlo@uptickhq.com>
2019-03-04 16:56:58 +1100

    babel config

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function(api) {
+  api.cache(true)
+
+  const presets = [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ]
+
+  const plugins = [
+    '@babel/plugin-proposal-export-default-from',
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
+  ]
+
+  return {
+    presets,
+    plugins,
+  }
+}
