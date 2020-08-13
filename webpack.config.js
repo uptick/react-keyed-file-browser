1af119b12cf77afa0b88cfc6f38688527c238b03
Oliver Foster <olivermartinfoster@gmail.com>
2020-08-13 14:32:12 +0100

    issue/136 webpack.config.js include for babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,8 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        loader: ['babel-loader'],
+        include: path.join(__dirname, './src/'),
       },
       {
         test: /.*\.sass*/,
