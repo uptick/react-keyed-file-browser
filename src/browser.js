75783b1e7a655ea0874f6bd6b8c5bd32b4b1bcea
Tomek Marcinkowski <tmarcinkowski@logitech.com>
2021-06-19 12:09:12 +0200

    separate action bar renderer

--- src/browser.js
+++ src/browser.js
@@ -19,7 +19,7 @@ import { GroupByFolder } from './groupers'
 import { SortByName } from './sorters'
 
 import { isFolder } from './utils'
-import { DefaultAction } from './actions'
+import { DefaultAction, DefaultActionBar } from './actions'
 
 const SEARCH_RESULTS_PER_PAGE = 20
 const regexForNewFolderOrFileSelection = /.*\/__new__[/]?$/gm
@@ -83,6 +83,7 @@ class RawFileBrowser extends React.Component {
     detailRenderer: PropTypes.func,
     detailRendererProps: PropTypes.object,
     actionRenderer: PropTypes.func,
+    actionBarRenderer: PropTypes.func,
     confirmDeletionRenderer: PropTypes.func,
     confirmMultipleDeletionRenderer: PropTypes.func,
 
@@ -133,6 +134,7 @@ class RawFileBrowser extends React.Component {
     detailRenderer: DefaultDetail,
     detailRendererProps: {},
     actionRenderer: DefaultAction,
+    actionBarRenderer: DefaultActionBar,
     confirmDeletionRenderer: DefaultConfirmDeletion,
     confirmMultipleDeletionRenderer: MultipleConfirmDeletion,
 
@@ -536,6 +538,7 @@ class RawFileBrowser extends React.Component {
     const {
       icons, canFilter,
       filterRendererProps, filterRenderer: FilterRenderer,
+      actionBarRenderer: ActionBarRenderer,
       actionRenderer: ActionRenderer,
       onCreateFolder, onRenameFile, onRenameFolder,
       onDeleteFile, onDeleteFolder, onDownloadFile,
@@ -587,12 +590,7 @@ class RawFileBrowser extends React.Component {
       />
     )
 
-    return (
-      <div className="action-bar">
-        {filter}
-        {actions}
-      </div>
-    )
+    return <ActionBarRenderer filter={filter} actions={actions} />
   }
 
   renderFiles(files, depth) {
